62b918c09951e96368ac742ba962be6aca1a6852
Safee Adnan <safeeadnan@gmail.com>
2024-03-21 02:23:12 +0530

    1st commit

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+
+
+function App() {
+    const [a,seta]=useState(0);
+    const clickk = ()=>{seta(a+1)}
+  return (
+    <div>
+      <h1>💖 Hello World!</h1>
+      <p>Welcome to your Electron application. hein sachme</p>
+      <>idhar counter rakhte {a}</>
+      <button onClick={clickk}>abc</button>
+    </div>
+  );
+}
+
+export default App;
